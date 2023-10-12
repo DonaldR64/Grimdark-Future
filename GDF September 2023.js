a74e254e0c7cf9a858d1645bb54183bf0f1907f6
Don <Don.rhodes@icloud.com>
2023-10-12 14:43:16 -0400

    version

--- GDF September 2023.js
+++ GDF September 2023.js
@@ -1,5 +1,5 @@
 const GDF = (()=> {
-    const version = '1.10.11';
+    const version = '1.10.12';
     const rules = '3.1.0'
     if (!state.GDF) {state.GDF = {}};
     const pageInfo = {name: "",page: "",gridType: "",scale: 0,width: 0,height: 0};
